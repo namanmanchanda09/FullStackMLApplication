79423e3b24a2fcae381c0b1cfaf3dba5193abf34
namanmanchanda09 <naman.mohan09@outlook.com>
2020-02-13 12:58:36 +0530

    fixed error on my_example.py

--- my_example.py
+++ my_example.py
@@ -1,15 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import numpy as np
+import json
 from tensorflow.keras.models import load_model
 import joblib
+
+from json import JSONEncoder
+
+
+# class NumpyArrayEncoder(JSONEncoder):
+#     def default(self, obj):
+#         if isinstance(obj, numpy.ndarray):
+#             return obj.tolist()
+#         return JSONEncoder.default(self, obj)
 
 def return_prediction(model,scaler,sample_json):
     
     
-    s_len = sample_json['sepal_length']
-    s_wid = sample_json['sepal_width']
-    p_len = sample_json['petal_length']
-    p_wid = sample_json['petal_width']
+    s_len = sample_json["sepal_length"]
+    s_wid = sample_json["sepal_width"]
+    p_len = sample_json["petal_length"]
+    p_wid = sample_json["petal_width"]
     
     flower=[[s_len,s_wid,p_len,p_wid]]
     classes = np.array(['setosa', 'versicolor', 'virginica'])
@@ -26,7 +36,19 @@ def index():
 flower_model = load_model('final_iris_model.h5')
 flower_scaler = joblib.load('iris_scaler.pkl')
 
+@app.route("/api/flower",methods=['POST'])
+def flower_prediction():
+    content = request.json
+    
+    results = return_prediction(flower_model,flower_scaler,content)
+    return str(results)
+
+
 if __name__ == '__main__':
     app.run()
+
+
+
+
 
 
